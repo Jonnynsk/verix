6a31e6304b54cac4f0bc049a600382cf5891cfb4
Jonnynsk <@email.com>
2026-04-05 21:51:48 +0700

    Updates

---
+++ _next/static/YMYZNIQ6sKl0dsRWBp2Dl/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/verix//_next/:path+",
+        "destination": "/verix/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
