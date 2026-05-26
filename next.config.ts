d1ea294964362b7ae27421a98966548b28d6c84c
Jonnynsk <@email.com>
2026-05-26 20:20:44 +0700

    feat: github workflow

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,8 @@
 import type { NextConfig } from "next";
 
-const basePath = process.env.NODE_ENV === "development" ? "" : "/verix";
-
 const nextConfig: NextConfig = {
   reactCompiler: true,
-  env: {
-    NEXT_PUBLIC_BASE_PATH: basePath,
-  },
-  output: "export",
-  ...(basePath ? { basePath, assetPrefix: `${basePath}/` } : {}),
+  output: "export", 
   trailingSlash: true,
   images: {
     unoptimized: true,
